9a17930282923875f8d5db4f7067a1982d3fe5a8
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2024-12-21 02:42:03 +0630

    home service section static

---
+++ src/scripts/data/html.js
@@ -0,0 +1,20 @@
+export const serviceCards = [
+    {
+        iTag: "<i class=\"bi bi-bicycle text-[6rem] text-dark mb-4\"></i>",
+        title: "Delicious Food, Delivered Fast",
+        content: "Don't waste your time waiting—savor your favorites in no time!",
+        btnContent: "Order Now"
+    },
+    {
+        iTag: "<i class=\"bi bi-egg-fried text-[6rem] text-dark mb-4\"></i>",
+        title: "FDA Approved Ingredients",
+        content: "Our dishes are crafted with care, meeting the highest safety and quality standards.",
+        btnContent: "Learn More"
+    },
+    {
+        iTag: "<i class=\"bi bi-hand-thumbs-up text-[6rem] text-dark mb-4\"></i>",
+        title: "Excellent Guest Satisfaction",
+        content: "We prioritize your happiness with every meal we serve.",
+        btnContent: "Read Reviews"
+    }
+];
---
+++ src/scripts/home.js
@@ -0,0 +1,26 @@
+import { serviceCards } from "./data/html.js";
+
+// render service cards
+
+const homeServiceSection = document.getElementById("js-home-services-sec");
+let cardHTML = "";
+
+serviceCards.forEach((card) => {
+  cardHTML += `
+    <div class="js-service-card flex flex-col items-center justify-center flex-1 p-4 max-w-[300px] text-center shadow-md rounded">
+        <div>
+            ${card.iTag}
+        </div>
+        <div>
+            <h1 class="text-xl font-bold mb-2">${card.title}</h1>
+            <p class="text-dark-700">${card.content}</p>
+        </div>
+        <div class="h-40">
+            <button class="mt-5 p-2 text-lg border-2 border-[2px] border-dotted border-accent text-accent rounded hover:bg-accent hover:text-dark transition">
+                ${card.btnContent} <i class="bi bi-arrow-bar-right"></i>
+            </button>
+        </div>
+    </div>`;
+});
+
+homeServiceSection.innerHTML = cardHTML;
