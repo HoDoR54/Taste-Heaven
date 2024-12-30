78439d11d234d18bbf7de78df4f16678ac9f2f29
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2024-12-30 17:28:37 +0630

    [Render] -- render all the menu items on menu page (filters not yet included)

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,17 +1,45 @@
 import { menuItems } from "./data/menuItems.js";
+import { setViewingDish } from "./data/dish-view.js";
 
-const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-const favoriteFilter = document.getElementById("js-menu-favorites");
-const menuDisplay = document.getElementById("js-menu-display");
+// render the cards on page
 
-favoriteFilter.addEventListener("click", () => {
-  document.title = `Faorites - Menu`;
-  let menuDisplayHtml = "";
-  favorites.forEach((favorite) => {
-    const favoriteDish = menuItems.find((dish) => dish.dishId === favorite);
-    menuDisplayHtml += `
-      <span class="font-bold text-xl">${favoriteDish.dishName} (${favoriteDish.dishId})</span>
-    `;
+const menuDisplayDiv = document.getElementById("js-menu-display");
+let menuHtml = "";
+
+menuItems.forEach((dish) => {
+  menuHtml += `
+    <a
+    href="dish-details.html"
+    data-dish-id="${dish.dishId}"
+    data-dish-category="${dish.category}"
+    class="relative overflow-hidden rounded-md cursor-pointer js-dish-view h-max group"
+    >
+      <div>
+        <img
+          src="../images/menu/${dish.dishPic}"
+          alt="${dish.alt}"
+          class="w-full aspect-square group-hover:brightness-75 object-cover"
+        />
+      </div>
+      <div class="absolute bottom-0 left-0 right-0 p-2 text-primary bg-gradient-to-t from-dark to-[rgba(0, 0, 0, 0.5)]">
+        ${dish.dishName}
+      </div>
+    </a>
+  `;
+});
+
+menuDisplayDiv.innerHTML = menuHtml;
+
+// connect the page to dish details page
+
+const viewBtns = document.querySelectorAll(".js-dish-view");
+
+viewBtns.forEach((viewBtn) => {
+  viewBtn.addEventListener("click", () => {
+    const clickedDishId = viewBtn.getAttribute("data-dish-id");
+    const selectedDish = menuItems.find(
+      (dish) => dish.dishId === clickedDishId
+    );
+    setViewingDish(selectedDish);
   });
-  menuDisplay.innerHTML = menuDisplayHtml;
 });
