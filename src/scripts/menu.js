979674e2e30439073c9eab448bcbdf8952c9696b
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2024-12-30 21:19:18 +0630

    [Add] -- finalize the menu page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,34 +1,119 @@
 import { menuItems } from "./data/menuItems.js";
 import { setViewingDish } from "./data/dish-view.js";
+import { addToFavToggle } from "./UX/add-to-fav.js";
 
 // render the cards on page
-
 const menuDisplayDiv = document.getElementById("js-menu-display");
+const categoryName = document.getElementById("js-menu-category-display");
 let menuHtml = "";
 
-menuItems.forEach((dish) => {
-  menuHtml += `
-    <a
-    href="dish-details.html"
-    data-dish-id="${dish.dishId}"
-    data-dish-category="${dish.category}"
-    class="relative overflow-hidden rounded-md cursor-pointer js-dish-view h-max group"
-    >
-      <div>
-        <img
-          src="../images/menu/${dish.dishPic}"
-          alt="${dish.alt}"
-          class="w-full aspect-square group-hover:brightness-75 object-cover"
-        />
+function displayDish(title, array) {
+  menuHtml = "";
+  menuDisplayDiv.innerHTML = "";
+  categoryName.textContent = title;
+  array.forEach((dish) => {
+    categoryName.textContent = title;
+    menuHtml += `
+      <div
+      href="dish-details.html"
+      data-dish-category="${dish.category}"
+      class="relative overflow-hidden rounded-md cursor-pointer h-max group"
+      >
+        <a href="dish-details.html"
+        data-dish-id="${dish.dishId}"
+        class="js-dish-view">
+          <img
+            src="../images/menu/${dish.dishPic}"
+
+            alt="${dish.alt}"
+            class="w-full aspect-square group-hover:brightness-75 object-cover"
+          />
+        </a>
+        <div class="absolute bottom-0 left-0 right-0 p-2 text-primary bg-gradient-to-t from-dark to-[rgba(0, 0, 0, 0.5)]">
+          ${dish.dishName}
+        </div>
+        <div
+        class="absolute top-0 right-0 z-10 flex p-2 rounded-none bg-primary text-dark"
+        >
+            <i
+                class="text-lg cursor-pointer add-to-favorites fa-regular fa-heart" data-dish-id="${dish.dishId}"
+            ></i>
+            <i
+                class="hidden text-lg cursor-pointer added-to-favorites fa-solid fa-heart" data-dish-id="${dish.dishId}"
+            ></i>
+        </div>
       </div>
-      <div class="absolute bottom-0 left-0 right-0 p-2 text-primary bg-gradient-to-t from-dark to-[rgba(0, 0, 0, 0.5)]">
-        ${dish.dishName}
-      </div>
-    </a>
-  `;
+    `;
+    menuDisplayDiv.innerHTML = menuHtml;
+  });
+
+  // enable the 'add to favorite' toggle
+
+  const addToFavoritesIcons = document.querySelectorAll(".add-to-favorites");
+  const addedToFavoritesIcons = document.querySelectorAll(
+    ".added-to-favorites"
+  );
+
+  addToFavToggle(addToFavoritesIcons, addedToFavoritesIcons);
+}
+displayDish("All items in the menu", menuItems);
+
+// enable radio-like checked function for categories
+
+const categoryBtns = document.querySelectorAll(".js-menu-filter");
+
+categoryBtns.forEach((category) => {
+  category.addEventListener("click", () => {
+    categoryBtns.forEach((cat) => {
+      if (cat.classList.contains("bg-accent")) {
+        cat.classList.remove("bg-accent");
+        cat.classList.add("bg-primary");
+      }
+    });
+    category.classList.add("bg-accent");
+    category.classList.remove("bg-primary");
+  });
 });
 
-menuDisplayDiv.innerHTML = menuHtml;
+// filter the menu via click
+
+categoryBtns.forEach((category) => {
+  category.addEventListener("click", () => {
+    menuDisplayDiv.innerHTML = "";
+    const categoryName = category.getAttribute("data-filter-name");
+    let filteredDishes = [];
+    const filteringFunc = () => {
+      filteredDishes = menuItems.filter(
+        (dish) => dish.category === categoryName
+      );
+    };
+    switch (categoryName) {
+      case "All":
+        filteredDishes = menuItems;
+        break;
+      case "Main Courses":
+      case "Appetizers":
+      case "Beverages":
+      case "Kids' Menu":
+      case "Sides":
+        filteringFunc();
+        break;
+      case "Highest Rated":
+        filteredDishes = menuItems.filter((dish) => dish.rating >= 4.5);
+        break;
+      case "Favorites":
+        const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+        favorites.forEach((favorite) => {
+          const favoriteDish = menuItems.find(
+            (dish) => dish.dishId === favorite
+          );
+          filteredDishes.push(favoriteDish);
+        });
+        break;
+    }
+    displayDish(categoryName, filteredDishes);
+  });
+});
 
 // connect the page to dish details page
 
