322a0199ff662bd7c311911dd8c1a7d14b3c640a
Hpone Tauk Nyi <hponetaukyou@gmail.com>
2025-01-13 00:33:04 +0630

    [Add] -- interactive Order DOM in progress

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -1,11 +1,41 @@
 import { OrderManipulation } from "./data/order-dishes.js";
 import { menuItems } from "./data/menuItems.js";
 import { renderGeneralElements } from "./data/general-html.js";
+import { formatCurrency } from "./utils/money.js";
 
 renderGeneralElements("order");
 
 let orderMani = new OrderManipulation();
 const orders = orderMani.getOrderList();
 const orderedDishes = menuItems.filter((dish) => orders.includes(dish.dishId));
+const orderedDishesContainer = document.getElementById("js-ordered-dishes");
+let orderedDishesHtml = "";
 
-orderedDishes.forEach((orderedDish) => {});
+orderedDishes.forEach((orderedDish) => {
+  orderedDishesHtml += `
+        <div class="grid grid-cols-4 py-3 mb-4 js-ordered-dish" data-dish-id="${
+          orderedDish.dishId
+        }">
+            <div
+            class="relative flex items-center justify-center overflow-hidden rounded-md h-[150px]"
+            >
+            <img
+                src="../images/menu/${orderedDish.dishPic}"
+                alt="${orderedDish.alt}"
+                class="object-cover w-full h-full"
+            />
+            <span
+                class="absolute bottom-0 flex items-center justify-center w-full text-lg text-primary bg-gradient-to-t from-dark to-transparent"
+                >${orderedDish.dishName}</span
+            >
+            </div>
+            <div class="flex items-center justify-center">$ ${formatCurrency(
+              orderedDish.price
+            )}</div>
+            <div class="flex items-center justify-center">Haha</div>
+            <div class="flex items-center justify-center">Hehe</div>
+        </div>
+    `;
+});
+
+orderedDishesContainer.innerHTML = orderedDishesHtml;
